b16e55fa7140dc6b6f30f4000c0fc1c3de40b205
officialcody <sparshgoelt26@gmail.com>
2021-09-21 01:11:25 +0530

    Added show page for a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  def show
+    task = Task.find_by(slug: params[:slug])
+    if task
+      render status: :ok, json: { task: task }
+    else
+      render status: :not_found, json: { error: t("task.not_found") }
+    end
+  end
+
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: tasks }
