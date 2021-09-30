8e9b65d7944b1909fa09dfe0f4c2635591af6f78
officialcody <sparshgoelt26@gmail.com>
2021-09-30 15:58:25 +0530

    Added test cases for sidekiq email sending feature

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,7 +26,7 @@ class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
   # Run tests in parallel with specified workers
   # parallelize(workers: :number_of_processors) unless ENV["COVERAGE"]
-
+  Dir[Rails.root.join("test/support/**/*.rb")].each { |f| require f }
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
   def headers(user, options = {})
